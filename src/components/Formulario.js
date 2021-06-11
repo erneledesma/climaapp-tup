865f2920376244aa223b9eb0e73c5562652e0ff5
Juan Pablo Loureyro <juanpabloloureyro@gmail.com>
2021-06-11 18:10:47 -0300

    fixes

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,83 +1,74 @@
-import React, { useState } from 'react';
-import Error from './Error';
+import React from "react";
+import Error from "./Error";
 
-const Formulario = ({ busqueda, guardarBusqueda, guardarConsulta }) => {
+const Formulario = ({
+  consultarApi,
+  busqueda,
+  guardarBusqueda,
+  error,
+  guardarError,
+}) => {
+  // extraer ciudad y el pais
+  const { ciudad } = busqueda;
 
-    
-    const [error, guardarError] = useState(false);
+  // funcion que coloca los elementos en el state
+  const handleChange = (e) => {
+    //actualizar el state
+    guardarBusqueda({
+      ...busqueda,
+      [e.target.name]: e.target.value,
+    });
+  };
 
-      // extraer ciudad y el pais
-      const { ciudad, pais } = busqueda;
-    // funcion que coloca los elementos en el state
-
-    const handleChange = e => {
-        //actualizar el state
-        guardarBusqueda({
-            ...busqueda,
-            [e.target.name]:e.target.value
-        });
+  // Cuando el user da submit al formulario
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // validar
+    if (ciudad.trim() === "") {
+      return guardarError(true);
     }
+    guardarError(false);
 
-    // Cuando el user da submit al formulario
-    const handleSubmit = e => {
-        e.preventDefault();
+    //llamada a la api y si se resuelve correctamente la Promise se limpia el formulario
+    consultarApi().then(guardarBusqueda({ ciudad: "" }));
+  };
 
-        // validar
-        if(ciudad.trim() === '' || pais.trim() === '' ){
-            guardarError(true)
-            return;
-        }
-        guardarError(false);
+  return (
+    <form onSubmit={handleSubmit}>
+      {error ? <Error mensaje="Ambos campos son obligatorios" /> : null}
+      <div className="input-field col s12">
+        <input
+          type="text"
+          name="ciudad"
+          id="ciudad"
+          value={ciudad}
+          onChange={handleChange}
+        />
+        <label htmlFor="ciudad">Ciudad:</label>
+      </div>
 
-        guardarConsulta(true)
-        
-    }
+      <div className="input-field col s12">
+        <select name="pais" onChange={handleChange}>
+          <option selected>--Seleccione un pais--</option>
+          <option value="AR">Argentina</option>
+          <option value="US">Estados Unidos</option>
+          <option value="MX">Mexico</option>
+          <option value="CO">Colombia</option>
+          <option value="ES">España</option>
+          <option value="PE">Peru</option>
+        </select>
+      </div>
 
-    return ( 
-       <form
-       onSubmit={handleSubmit}
+      <div className="input-field col s12">
+        <button
+          type="submit"
+          className="waves-effect waves-light btn-large btn-block yellow accent-4 button"
         >
-       { error ? <Error mensaje='Ambos cmapos son obligatorios' /> : null }
-      
-           <div className="input-field col s12">
-               <input
-                type="text"
-                name="ciudad"
-                id="ciudad"
-                value={ciudad}
-                onChange={handleChange}
-                />
+          Buscar clima
+        </button>
+      </div>
+    </form>
+  );
+};
 
-                <label htmlFor="ciudad">Ciudad:</label>
-           </div>
-
-           <div className="input-field col s12">
-               <select
-                name="pais"
-                id="pais"
-                value="pais"
-                onChange={handleChange}
-               >
-                   <option value="">--Selecione un pais--</option>
-                   <option value="US">Estados Unidos</option>
-                   <option value="MX">Mexico</option>
-                   <option value="AR">Argentina</option>
-                   <option value="CO">Colombia</option>
-                   <option value="ES">España</option>
-                   <option value="PE">Peru</option>
-               </select>
-           </div>
-
-           <div className="input-field col s12">
-            <input
-                type="submit"
-                value="Buscar clima"
-                className="waves-effect waves-light btn-large btn-block yellow accent-4"
-             />
-
-           </div>
-       </form>
-     );
-}
- 
 export default Formulario;
